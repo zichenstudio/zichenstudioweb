6a69e3062ebfa46466e0b97126a81019803b5c69
ZiChenStudio <zichenstudio@outlook.com>
2023-06-25 23:43:04 +0800

    Update vim-control.js
    添加了网页可以Vim快捷键控制的功能

--- js/getscript.js
+++ js/getscript.js
@@ -1,4 +1,4 @@
-/***
+/**
  * 此JavaScript文件的作用是让网页使用getScript的方法获取其他的JavaScript文件
  * 放的所有的JavaScript文件都是几乎大多数网页都能用到的
  * 不会放只有一两个网站能用到的JavaScript文件
@@ -10,7 +10,10 @@ $.getScript("/js/copyright.js");
 
 // highlight.js文件
 // 顺次加载
-$.getScript('https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/highlight.min.js', function() {
+$.getScript('https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/highlight.min.js', function () {
     $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/default.min.css">');
     hljs.highlightAll();
 });
+
+// vim-control.js文件
+$.getScript("/js/vim-control.js");
