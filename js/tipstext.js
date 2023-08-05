7b90fb6d769c84fadc28d070c5b01499710b1565
ZiChenStudio <zichenstudio@outlook.com>
2023-08-06 01:18:35 +0800

    Refactor tipstext.js
    重构tipstext.js的代码

--- js/tipstext.js
+++ js/tipstext.js
@@ -3,9 +3,9 @@ $.ajax({
     dataType: 'json',
     async: false,
     success: function (data) {
-        tipsJsonText = data;
-        tipsJsonTextNum = tipsJsonText.length - 1;
-        console.log('成功获取到JSON数据：', tipsJsonText); // 在这里输出调试信息
+        tipsData = data;
+        tipsDataCount = tipsData.length - 1;
+        console.log('成功获取到JSON数据：', tipsData); // 在这里输出调试信息
     },
     error: function (xhr, status, error) {
         console.log('ajax请求失败:', status, xhr, error); // 在这里输出错误信息
@@ -13,32 +13,31 @@ $.ajax({
 });
 
 // 初始化变量
-let tipsTextWriteAutoStop = false;
-let egg = 0;
-let tipsTextWriteStop = false;
+let easterEggCount = 0;
+let writeStop = false;
 
 // 显示彩蛋信息
 let tipsTextEgg = () => {
-    if (egg >= 100) {
+    if (easterEggCount >= 100) {
         $('.tipsTextEgg').html('<span style="color:#F00;">都100条了还不够吗?</span>');
     }
-    if (egg >= 1000) {
+    if (easterEggCount >= 1000) {
         $('.tipsTextEgg').html('<span style="color:#F00;">你真的很闲！</span>');
     }
 };
 
 // 显示提示文本
 let tipsTextWrite = () => {
-    let random = Math.floor(Math.random() * tipsJsonTextNum);
-    $('.tipsText').html(tipsJsonText[random].tips);
-    console.log('引索:' + random + '个' + '|' + '文件第:' + (randomx + 2) + '行');
-    egg++;
+    let randomIndex = Math.floor(Math.random() * tipsDataCount);
+    $('.tipsText').html(tipsData[randomIndex].tips);
+    console.log('引索:' + randomIndex + '个' + '|' + '文件第:' + (randomIndex + 2) + '行');
+    easterEggCount++;
     tipsTextEgg();
 };
 
 // 自动显示提示文本
 function tipsTextWriteAuto() {
-    if (!tipsTextWriteStop && (tipsTextWriteStop = true)) {
+    if (!writeStop && (writeStop = true)) {
         tipsTextWrite();
     }
     setTimeout(() => {
@@ -51,9 +50,8 @@ function tipsTextWriteAuto() {
 tipsTextWrite();
 
 // 双击事件
-$('.tipsText').dblclick(_ => {
-    if (!tipsTextWriteStop) {
+$('.tipsText').dblclick(function() {
+    if (!writeStop) {
         tipsTextWrite();
     }
 });
-
