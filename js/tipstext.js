fa91339e37a559f12643928b6295b33933d0fa25
ZiChenStudio <zichenstudio@outlook.com>
2023-08-06 00:46:27 +0800

    fix tipsBug
    修复提示语无法使用自动切换功能的bug

--- js/tipstext.js
+++ js/tipstext.js
@@ -1,65 +1,59 @@
-$(document).ready(function() {
-    // 获取提示文本数据
+$.ajax({
+    url: '/js/tipstextjson.json',
+    dataType: 'json',
+    async: false,
+    success: function (data) {
+        tipsJsonText = data;
+        tipsJsonTextNum = tipsJsonText.length - 1;
+        console.log('成功获取到JSON数据：', tipsJsonText); // 在这里输出调试信息
+    },
+    error: function (xhr, status, error) {
+        console.log('ajax请求失败:', status, xhr, error); // 在这里输出错误信息
+    }
+});
 
-    $.ajax({
-        url: '/js/tipstextjson.json',
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-            tipsJsonText = data;
-            tipsJsonTextNum = tipsJsonText.length - 1;
-            console.log('成功获取到JSON数据：', tipsJsonText); // 在这里输出调试信息
-        },
-        error: function(xhr, status, error) {
-            console.log('ajax请求失败:', status, xhr, error); // 在这里输出错误信息
-        }
-    });
+// 初始化变量
+let tipsTextWriteAutoStop = false;
+let egg = 0;
+let tipsTextWriteStop = false;
 
+// 显示彩蛋信息
+let tipsTextEgg = () => {
+    if (egg >= 100) {
+        $('.tipsTextEgg').html('<span style="color:#F00;">都100条了还不够吗?</span>');
+    }
+    if (egg >= 1000) {
+        $('.tipsTextEgg').html('<span style="color:#F00;">你真的很闲！</span>');
+    }
+};
 
-    // 初始化变量
-    let tipsTextWriteAutoStop = false;
-    let egg = 0;
-    let tipsTextWriteStop = false;
+// 显示提示文本
+let tipsTextWrite = () => {
+    let random = Math.floor(Math.random() * tipsJsonTextNum);
+    $('.tipsText').html(tipsJsonText[random].tips);
+    console.log('引索:' + random + '个' + '|' + '文件第:' + (randomx + 2) + '行');
+    egg++;
+    tipsTextEgg();
+};
 
-    // 显示彩蛋信息
-    let tipsTextEgg = () => {
-        if (egg >= 100) {
-            $('.tipsTextEgg').html('<span style="color: #f00;">都100条了还不够吗?</span>');
-        }
-        if (egg >= 1000) {
-            $('.tipsTextEgg').html('<span style="color: #f00;">你真的很闲！</span>');
-        }
-    };
+// 自动显示提示文本
+function tipsTextWriteAuto() {
+    if (!tipsTextWriteStop && (tipsTextWriteStop = true)) {
+        tipsTextWrite();
+    }
+    setTimeout(() => {
+        tipsTextWrite();
+        tipsTextWriteAuto();
+    }, 3000);
+};
 
-    // 显示提示文本
-    let tipsTextWrite = () => {
-        let randomIndex = Math.floor(Math.random() * tipsJsonTextNum);
-        $('.tipsText').html(tipsJsonText[randomIndex].tips);
-        console.log('引索:' + randomIndex + '个' + '|' + '文件第:' + (randomIndex + 2) + '行');
-        egg++;
-        tipsTextEgg();
-    };
+// 初始化
+tipsTextWrite();
 
-    // 自动显示提示文本
-    function tipsTextWriteAuto() {
-        if (!tipsTextWriteStop && (tipsTextWriteStop = true)) {
-            tipsTextWrite();
-        }
-        setTimeout(() => {
-            tipsTextWrite();
-            tipsTextWriteAuto();
-        }, 3000);
-    };
-
-    // 初始化
-    tipsTextWrite();
-
-    // 双击事件
-    $('.tipsText').dblclick(_ => {
-        if (!tipsTextWriteStop) {
-            tipsTextWrite();
-        }
-    });
-
+// 双击事件
+$('.tipsText').dblclick(_ => {
+    if (!tipsTextWriteStop) {
+        tipsTextWrite();
+    }
 });
 
