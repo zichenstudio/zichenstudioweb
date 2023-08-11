49ad72531d9fbec35091951028de8f6b8c7120c5
ZiChenStudio <zichenstudio@outlook.com>
2023-08-12 02:03:28 +0800

    fix hitokoto.js

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -1,11 +1,11 @@
 // 最大兼容性代码
-function gethitokoto(){
+function gethitokoto() {
     $.ajax({
         type: 'GET',
         url: 'https://v1.hitokoto.cn/?c=a&c=b&c=c&c=h&c=i&c=j&c=k&encode=json&charset=utf-8',
         dataType: 'json',
         async: false,
-        success (data) {
+        success(data) {
             $("#hitokoto_text").attr("href", "https://hitokoto.cn/?uuid=" + data.uuid);
             // 判断来源空值
             no_from_who = (data.from_who === null || data.from_who === undefined || data.from_who === '') ? 1 : 0;
@@ -15,16 +15,16 @@ function gethitokoto(){
 
             hitokotoText = (no_from_who === 1 && no_from === 1) ? String(data.hitokoto) :
                 (no_from_who === 1) ? String(data.hitokoto) + '------' + from :
-                (no_from === 1) ? String(data.hitokoto) + '------' + from_who :
-                                  String(data.hitokoto) + '------' + from_who + from;
+                    (no_from === 1) ? String(data.hitokoto) + '------' + from_who :
+                        String(data.hitokoto) + '------' + from_who + from;
             // 一个漂亮的输出
             $('#hitokoto_text').text(hitokotoText);
             delete hitokotoText;
         },
     });
-}
+};
 function getHitokotoText() {
     $('#hitokoto_text').text('获取中……');
     setTimeout("gethitokoto()", 500);
-}
+};
 setTimeout("getHitokotoText()", 2000);
