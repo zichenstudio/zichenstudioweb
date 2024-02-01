24a87b6d6b1028977298b4bf41dee4b1e4db5c60
zichenstudio@outlook.com <zichenstudio@outlook.com>
2024-02-01 19:49:23 +0800

    Update Style

--- script/tipstext.js
+++ script/tipstext.js
@@ -1,26 +1,25 @@
-const tipsTextElement = document.querySelector(".tipsText");
-const tipsTextEggElement = document.querySelector('.tipsTextEgg');
+const tipsTextElement = document.querySelector(".tips-text");
+const tipsTextEggElement = document.querySelector('.tips-text-egg');
 let clickCount = 0;
 async function fetchTips() {
-    const URL = window.location.origin;
-    try {
-        const response = await fetch(`${URL}/script/tipstext.json`);
-        const data = await response.json();
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomTip = data[randomIndex];
-        tipsTextElement.innerHTML = randomTip;
-    } catch (error) {
-        console.error('Error fetching tips:', error);
-    }
+  const URL = window.location.origin;
+  try {
+    const response = await fetch(`${URL}/script/tipstext.json`);
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomTip = data[randomIndex];
+    tipsTextElement.innerHTML = randomTip;
+  } catch (error) {
+    console.error('Error fetching tips:', error);
+  }
 }
 function handleDoubleClick() {
-    fetchTips();
-    clickCount++;
-    if (clickCount >= 100) {
-        tipsTextEggElement.innerHTML = `
-        <span style="color:#FF0000">已经100条了，还不够吗？</span>
-        `;
-    }
+  fetchTips();
+  clickCount++;
+  if (clickCount === 100) {
+    tipsTextEggElement.innerHTML = '<span style="color:#FF0000">已经100条了，还不够吗？</span>';
+  }
 }
 fetchTips();
 tipsTextElement.addEventListener("dblclick", handleDoubleClick);
+
